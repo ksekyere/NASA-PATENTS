9d0ef5c37c52b90b71f6baf33b8c74e6416838f4
ksekyere <58433475+ksekyere@users.noreply.github.com>
2024-06-10 20:02:32 -0500

    nasa word cloud

---
+++ Nasa Patent Word Cloud.R
@@ -0,0 +1,45 @@
+#helpful guides --------------------
+https://guides.library.upenn.edu/penntdm/r
+
+
+library(dplyr)
+library(paletteer)
+
+install.packages("wordcloud")
+library(wordcloud)
+install.packages("RColorBrewer")
+library(RColorBrewer)
+install.packages("wordcloud2")
+library(wordcloud2)
+
+install.packages("tm")
+library(tm)
+
+nasa.patents = read.csv("/Users/kofi/Desktop/NASA PATENTS/NASA_Patents.csv")
+
+
+patent.titles = nasa.patents$Title
+
+nasa.corpus = Corpus(VectorSource(patent.titles))
+
+nasa.corpus = tm_map(nasa.corpus, content_transformer(tolower))
+
+
+dtm <- TermDocumentMatrix(nasa.corpus) 
+matrix <- as.matrix(dtm) 
+words <- sort(rowSums(matrix),decreasing=TRUE) 
+df <- data.frame(word = names(words),freq=words) %>%
+filter(! word %in% c("and", "for", "method", "system", "using", "with", "from"))
+
+
+set.seed(1234) # for reproducibility 
+
+#get orange theme
+orange= paletteer_c("ggthemes::Orange", 30)
+
+
+
+#WORD CLOUD HERE
+wordcloud(words = df$word, freq = df$freq, min.freq = 1, 
+max.words=200, random.order=FALSE, rot.per=0.35, color = orange
+)
